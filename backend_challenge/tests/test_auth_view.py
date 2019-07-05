4b62d07efa6ab91ac441e2087020dee4d19834ee
Israel Silva <israel.silva@nexxera.com>
2019-07-05 09:15:16 -0300

    Including more testing on authentication

--- backend_challenge/tests/test_auth_view.py
+++ backend_challenge/tests/test_auth_view.py
@@ -12,9 +12,38 @@ def test_should_create_auth(customer_client):
     # GIVEN
     User.objects.create_superuser(username="test", email='test@test.com', password="test")
 
+    # WHEN
     response = customer_client.post(f'{RESOURCE_PATH}/auth', data={'username': 'test', 'password': "test"})
 
+    # THEN
     assert response.status_code == HTTPStatus.OK
 
     content = json.loads(response.content.decode())
     assert type(content) == dict
+
+
+def test_should_not_create_auth_without_super_user(customer_client):
+    # GIVEN
+
+    # WHEN
+    response = customer_client.post(f'{RESOURCE_PATH}/auth', data={'username': 'test', 'password': "test"})
+
+    # THEN
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+    content = json.loads(response.content.decode())
+    assert content.get('detail') == 'No active account found with the given credentials'
+
+
+def test_should_not_create_auth_with_invalid_credentials(customer_client):
+    # GIVEN
+    User.objects.create_superuser(username="test", email='test@test.com', password="test")
+
+    # WHEN
+    response = customer_client.post(f'{RESOURCE_PATH}/auth', data={})
+
+    # THEN
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+
+    content = json.loads(response.content.decode())
+    assert content == {'username': ['This field is required.'], 'password': ['This field is required.']}
